4d118073e74731722d41347f2922a5a40378109e
Md Shafikur Rahaman <58303621+mdshofiur@users.noreply.github.com>
2023-07-08 21:45:45 +0600

    main function remove

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,8 @@
-import express, { Express, NextFunction, Request, Response } from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
-import userRouter from "./modules/user/user.route";
-import cowRouters from "./modules/cow/cow.route";
-import ordersRouters from "./modules/orders/order.route";
-import adminRouter from "./modules/admin/admin.route";
+import app from "./app";
 
 const port: number = 2000;
 const uri: any = process.env.DB_URL || "mongodb://localhost:27017/firstdb";
-
-const app: Express = express();
-
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cookieParser());
-
-app.use(userRouter);
-app.use(cowRouters);
-app.use(ordersRouters);
-app.use(adminRouter);
-
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("Hello World!");
-});
 
 app.listen(port, async () => {
   try {
