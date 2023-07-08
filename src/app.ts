d4e08e0976a357e35f8d58ef182a9c494e2917e8
Md Shafikur Rahaman <58303621+mdshofiur@users.noreply.github.com>
2023-07-08 21:42:42 +0600

    app.ts code move in server.ts

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,29 @@
-import express, { Express, NextFunction, Request, Response } from "express";
-import cors from "cors";
-import cookieParser from "cookie-parser";
-import "dotenv/config";
-import { errorHandlerMiddleware } from "./middleware/error-handler";
-import userRouter from "./modules/user/user.route";
-import cowRouters from "./modules/cow/cow.route";
-import ordersRouters from "./modules/orders/order.route";
-import adminRouter from "./modules/admin/admin.route";
+// import express, { Express, NextFunction, Request, Response } from "express";
+// import cors from "cors";
+// import cookieParser from "cookie-parser";
+// import "dotenv/config";
+// import { errorHandlerMiddleware } from "./middleware/error-handler";
+// import userRouter from "./modules/user/user.route";
+// import cowRouters from "./modules/cow/cow.route";
+// import ordersRouters from "./modules/orders/order.route";
+// import adminRouter from "./modules/admin/admin.route";
 
-const app: Express = express();
-require("dotenv").config();
+// const app: Express = express();
+// require("dotenv").config();
 
-app.use(cors());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(errorHandlerMiddleware);
-app.use(cookieParser());
+// app.use(cors());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(errorHandlerMiddleware);
+// app.use(cookieParser());
 
 // app.use(userRouter);
 // app.use(cowRouters);
 // app.use(ordersRouters);
 // app.use(adminRouter);
 
+// app.get("/", (req: Request, res: Response, next: NextFunction) => {
+//   res.send("Hello World!");
+// });
 
-
-export default app;
+// export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,35 +1,37 @@
-import mongoose from "mongoose";
-import app from "./app";
 import express, { Express, NextFunction, Request, Response } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
 import userRouter from "./modules/user/user.route";
 import cowRouters from "./modules/cow/cow.route";
 import ordersRouters from "./modules/orders/order.route";
 import adminRouter from "./modules/admin/admin.route";
 
 const port: number = 2000;
+const uri: any = process.env.DB_URL || "mongodb://localhost:27017/firstdb";
 
-export default async function main() {
-  const uri: any = process.env.DB_URL || "mongodb://localhost:27017/firstdb";
-  try {
-    (await mongoose.connect(uri)) as any;
-    console.log("Database is connected");
-    // app listen
-    app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`);
-    });
-  } catch (error) {
-    console.log("Database connect error", error);
-  }
-}
+const app: Express = express();
 
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("Hello World!");
-});
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
 
 app.use(userRouter);
 app.use(cowRouters);
 app.use(ordersRouters);
 app.use(adminRouter);
 
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.send("Hello World!");
+});
 
-main();
+app.listen(port, async () => {
+  try {
+    await mongoose.connect(uri);
+    console.log("Database is connected");
+    console.log(`Example app listening on port ${port}`);
+  } catch (error) {
+    console.log("Database connect error", error);
+  }
+});
